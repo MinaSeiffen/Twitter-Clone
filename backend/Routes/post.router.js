cdea81e6eaa8ea75abdb0c8fc9f3a678e2006dab
MinaE.Seiffen <minaseiffen@gmail.com>
2024-04-26 20:22:09 +0300

    Post Controllers

---
+++ backend/Routes/post.router.js
@@ -0,0 +1,17 @@
+import express from "express"
+import { protect } from "../MiddleWare/Protect.js"
+import { commentOnPost, createPost, deletePost, getAllPosts, getFollowingPosts, getLikedPosts, getUserPosts, likeAndUnlikePost } from "../Controllers/post.controllers.js"
+
+const router = express.Router()
+
+router.get('/', protect , getAllPosts)
+router.get('/following', protect , getFollowingPosts)
+router.get('/likes/:id', protect , getLikedPosts)
+router.get('/user/:userName', protect , getUserPosts)
+router.post("/create" , protect , createPost)
+router.post("/like/:id" , protect , likeAndUnlikePost)
+router.post("/comment/:id" , protect , commentOnPost)
+router.delete("/:id" , protect , deletePost)
+
+
+export default router
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import {v2 as cloudinary} from "cloudinary"
 
 import authRoutes from "./Routes/auth.router.js"
 import userRoutes from "./Routes/user.router.js"
+import postRoutes from "./Routes/post.router.js"
 
 dotenv.config();
 
@@ -25,6 +26,7 @@ app.use(cookieParser())
 
 app.use("/api/auth" , authRoutes)
 app.use("/api/user" , userRoutes)
+app.use("/api/post" , postRoutes)
 
 
 app.listen(port , ()=>{
