7e5d7cd3d2e565875a5e9d591559411133c5c447
MinaE.Seiffen <minaseiffen@gmail.com>
2024-04-27 13:26:12 +0300

    UI Design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import SignUpPage from "./Pages/Auth/SignUp/SignUpPage";
+import LoginPage from "./Pages/Auth/Login/LoginPage";
+import HomePage from "./Pages/Home/HomePage";
+import SideBar from "./Components/Common/SideBar";
+import RightPanel from "./Components/Common/RightPanel";
+import NotificationPage from "./Pages/Notification/NotificationPage";
+import ProfilePage from "./Pages/Profile/ProfilePage";
 
-const App = () => {
-  return (
-    <>
-        
-    </>
-  )
+function App() {
+	return (
+		<div className='flex max-w-6xl mx-auto'>
+      <SideBar/>
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/signup' element={<SignUpPage />} />
+				<Route path='/login' element={<LoginPage />} />
+				<Route path='/notifications' element={<NotificationPage />} />
+				<Route path='/profile/:userName' element={<ProfilePage />} />
+			</Routes>
+      <RightPanel />
+		</div>
+	);
 }
 
 export default App
