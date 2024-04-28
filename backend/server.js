86f9e421834bd73fcffa2c28b2730483d3268ffd
MinaE.Seiffen <minaseiffen@gmail.com>
2024-04-28 21:55:19 +0300

    Building the Project for Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path"
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -19,6 +20,7 @@ cloudinary.config({
 
 const app = express();
 const port = process.env.PORT || 3000;
+const __dirname = path.resolve()
 
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
@@ -28,6 +30,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/notification", notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(port, () => {
   console.log("Server is Running on port " + port);
